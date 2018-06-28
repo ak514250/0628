3800e967a8ee9958c4650d1029977e727e3c0dcf
ak514250 <514250798@qq.com>
2018-06-28 16:01:35 +0800

    增加属性

---
+++ src/cn/du/test04.java
@@ -0,0 +1,16 @@
+package cn.du;
+
+/**
+ * Created by z on 2018/6/28.
+ */
+public class test04 {
+    private String dasd;
+
+    public String getDasd() {
+        return dasd;
+    }
+
+    public void setDasd(String dasd) {
+        this.dasd = dasd;
+    }
+}
